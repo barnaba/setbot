13f393f3e1263fc81d6c5cec8dd823f1858b0001
Barnaba Turek <barnabaturek@gmail.com>
2012-01-06 02:53:28 +0100

    setbot start script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
+rvm use 1.9.2@setbot
+cd /home/barnex/kod/setbot
+ruby setbot.rb
