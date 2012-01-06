b5711e157b57921f6ec45d4b852c415b7525c7c4
Barnaba Turek <barnabaturek@gmail.com>
2012-01-06 02:45:19 +0100

    porządki

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
 cd /home/barnex/kod/setbot
 curl -d "viewType=0&itemType=S&itemTypeInv=S&itemNo=&downloadType=T" http://www.bricklink.com/catalogDownload.asp\?a\=a | sed '1,3d' > sets.tsv
 sqlite3 sets.sqlite < load.sql
+rm sets.tsv
