b1ec49c57f75333ed3f56a38ef147ad9f9c93876
Barnaba Turek <barnabaturek@gmail.com>
2012-02-16 20:33:25 +0100

    db update script is now independent of your path

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,6 @@
-cd /home/barnex/kod/setbot
+#!/bin/bash
+BASEDIR=$(dirname $0)
+cd $BASEDIR
 curl -d "viewType=0&itemType=S&itemTypeInv=S&itemNo=&downloadType=T" http://www.bricklink.com/catalogDownload.asp\?a\=a | sed '1,3d' > sets.tsv
 sqlite3 sets.sqlite < load.sql
 rm sets.tsv
